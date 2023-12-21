d05343ea3382777a48c0d4ad3f60cd221c9fbf44
Jh <473380917@qq.com>
2023-12-21 22:11:16 +0800

    变现完成MyHashMap，支持动态扩容

--- src/com/my/utils/java/MyHashMap.java
+++ src/com/my/utils/java/MyHashMap.java
@@ -1,6 +1,8 @@
 package com.my.utils.java;
 
 
+import java.util.Arrays;
+
 public class MyHashMap<K,V> {
 
     public class Node<K,V>{
@@ -13,6 +15,14 @@ public class MyHashMap<K,V> {
             this.next = next;
         }
 
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", key=" + key +
+                    ", next=" + next +
+                    '}';
+        }
     }
 
     static final int DEFAULT_CAPACITY=10;
@@ -30,38 +40,98 @@ public class MyHashMap<K,V> {
         this.loadFactor = 0.75F;
         this.store =  (Node<K,V>[]) new Node[capacity];
     }
+    private int hash(K key){
+        return key.hashCode() % this.capacity;
+    }
+    private void expandCapacity(){
+        if(this.size+1>this.loadFactor*this.capacity){
+//            扩容
+            this.capacity = Math.round((1+this.loadFactor)*this.capacity);
+//          所有元素需要移动到新的store中
+            Node<K,V>[] newStore = (Node<K,V>[]) new Node[this.capacity];
+            for (Node<K,V> node: this.store){
+                while (node!=null){
+//                    位置交换过程
+                    Node<K,V> nextNode = node.next;
 
-    private Node insertNodeAtHead(Node LinkListHead, Node node){
-        node.next = LinkListHead;
-        node = LinkListHead;
-        return node;
+                    Node<K,V> headNode = newStore[hash(node.key)];
+                    newStore[hash(node.key)] = node;
+                    node.next = headNode;
+
+                    node = nextNode;
+                }
+            }
+
+            this.store = newStore;
+        }
     }
     public V put(K key,V value){
-        int index = key.hashCode() % this.capacity;
-        Node node = this.store[index];
+        return this.put(this.store,key,value);
+    }
+    private V put(Node<K,V>[] store,K key,V value){
 
+        Node node = store[hash(key)];
+//        先处理头节点
         if(node==null){
-            this.store[index] = new Node<>(key, value, null);
+            store[hash(key)] = new Node<>(key, value, null);
+            this.size++;
+            return value;
         }else{
-            while (node.next!=null){
-                if(node.key == key){
-                    node.value = value;
-                    return value;
-                }
-                node = node.next;
+            if(node.key==key){
+                node.value = value;
+                return value;
             }
         }
-
+//    遍历处理剩余节点
+        while (node.next!=null){
+            if(node.next.key == key){
+                node.next.value = value;
+                return value;
+            }
+            node = node.next;
+        }
+        this.expandCapacity();
         node.next = new Node<>(key, value, null);
+        this.size++;
         return value;
     }
 
     public V get(K key){
-        throw new RuntimeException("Not");
+        Node<K,V> node = this.store[hash(key)];
+        while (node!=null){
+            if(node.key ==key){
+                return node.value;
+            }else{
+                node = node.next;
+            }
+        }
+        return null;
     }
 
     public V remove(K key){
-        throw new RuntimeException("Not");
+        Node<K,V> node = this.store[hash(key)];
+//        不为空再处理
+        if(node!=null){
+//            处理头节点
+            if(node.key == key){
+                this.size--;
+                this.store[hash(key)]=node.next;
+                return node.value;
+            }
+//处理剩余节点
+            while (node.next!=null){
+                if(node.next.key ==key){
+                    Node<K,V> nextNode = node.next;
+                    node.next = node.next.next;
+                    return nextNode.value;
+                }else{
+                    node = node.next;
+                }
+            }
+        }
+
+
+        return null;
     }
 
     public static void main(String[] args) {
@@ -69,12 +139,32 @@ public class MyHashMap<K,V> {
         hashMap1.put("a",1);
         hashMap1.put("b",2);
         hashMap1.put("c",3);
-        hashMap1.put("a",5);
-//        // 5
-//        System.out.println(hashMap1.get("a"));
-//        // 5
-//        System.out.println(hashMap1.remove("a"));
-//        // null
-//        System.out.println(hashMap1.get("a"));
+        hashMap1.put("a",4);
+        hashMap1.put("l",5);
+        hashMap1.put("z",6);
+        hashMap1.put("h",7);
+        hashMap1.put("p",8);
+        hashMap1.put("o",9);
+        Arrays.stream(hashMap1.store).forEach((o)-> System.out.println(o));
+
+        System.out.println(hashMap1.get("b")==2);
+        System.out.println(hashMap1.get("c")==3);
+        System.out.println(hashMap1.get("a")==4);
+        System.out.println(hashMap1.get("l")==5);
+        System.out.println(hashMap1.get("z")==6);
+        System.out.println(hashMap1.get("h")==7);
+        System.out.println(hashMap1.get("p")==8);
+        System.out.println(hashMap1.get("o")==9);
+
+        hashMap1.remove("a");
+        hashMap1.remove("b");
+        hashMap1.remove("c");
+        hashMap1.remove("a");
+        hashMap1.remove("l");
+        hashMap1.remove("z");
+        hashMap1.remove("h");
+        hashMap1.remove("p");
+        hashMap1.remove("o");
+        Arrays.stream(hashMap1.store).forEach((o)-> System.out.println(o));
     }
 }
